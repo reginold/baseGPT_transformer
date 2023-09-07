3016fabe9d236a5dd6efcc220e0e20946bdce72a
Bowen Lu <reginold.lu@gmail.com>
2023-09-07 08:47:32 +0000

    add the generate func

--- gpt_dev.py
+++ gpt_dev.py
@@ -70,3 +70,52 @@ for b in range(batch_size):  # batch dimension
         context = xb[b, : t + 1]
         target = yb[b, t]
         print(f"when input is {context.tolist()} the target: {target}")
+
+print(xb)  # output to the transformer
+print("########################")
+
+################################
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+torch.manual_seed(1337)
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super(BigramLanguageModel, self).__init__()
+        # each token directly reads off the logits for the next token from a lookup table
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets):
+        # idx and targets are both(B, T) tensor of integers, C is the prediction of (B, T)
+        logits = self.token_embedding_table(idx)  # (B,T,C)
+
+        B, T, C = logits.shape
+        logits = logits.view(B * T, C)
+        targets = targets.view(B * T)
+        loss = F.cross_entropy(logits, targets)  # how well we predict the num
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the prediction
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1:]  # (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)  # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
+        return idx
+
+
+model = BigramLanguageModel(vocab_size)
+out, loss = model(xb, yb)
+print(out.shape)
+print(loss)
